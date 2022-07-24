a5d175b8a9184c34a8cdee45b083b6fedc24ba6a
nooose <hd15807@gmail.com>
2022-07-24 15:34:49 +0900

    #16 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 열지 테스트로 명세를 뽑는다.
    gradle 빌드 시 테스트 때문에 악 영향을 끼칠 수 있으므로 개발중인 테스트 기능은 비활성화함

--- src/main/java/com/project/board/controller/ArticleController.java
+++ src/main/java/com/project/board/controller/ArticleController.java
@@ -1,2 +1,10 @@
-package com.project.board.controller;public class ArticleController {
+package com.project.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
 }
