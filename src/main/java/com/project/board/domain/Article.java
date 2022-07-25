9738171241d3c21a5533f2f2f12061d08ab31c16
nooose <hd15807@gmail.com>
2022-07-26 01:18:18 +0900

    #19 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용

--- src/main/java/com/project/board/domain/Article.java
+++ src/main/java/com/project/board/domain/Article.java
@@ -10,7 +10,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -23,6 +23,8 @@ public class Article extends AuditingFields {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter @ManyToOne(optional = false)
+    private UserAccount userAccount;
     @Setter
     @Column(nullable = false)
     private String title;
@@ -32,22 +34,23 @@ public class Article extends AuditingFields {
     @Setter
     private String hashtag;
 
-    @OrderBy("id")
-    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     @ToString.Exclude
+    @OrderBy("createdAt DESC")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
     protected Article() {
     }
 
-    private Article(String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
-    public static Article of(String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
+        return new Article(userAccount, title, content, hashtag);
     }
 
     @Override
--- src/main/java/com/project/board/domain/ArticleComment.java
+++ src/main/java/com/project/board/domain/ArticleComment.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "content"),
         @Index(columnList = "createdAt"),
@@ -20,16 +20,19 @@ public class ArticleComment extends AuditingFields {
 
     @Setter @ManyToOne(optional = false)
     private Article article;
+    @Setter @ManyToOne(optional = false)
+    private UserAccount userAccount;
     @Setter @Column(nullable = false, length = 500)
     private String content;
 
-    private ArticleComment(Article article, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, String content) {
         this.article = article;
+        this.userAccount = userAccount;
         this.content = content;
     }
 
-    public static ArticleComment of(Article article, String content) {
-        return new ArticleComment(article, content);
+    public static ArticleComment of(Article article, UserAccount userAccount, String content) {
+        return new ArticleComment(article, userAccount, content);
     }
 
     @Override
