dee8c572b1226f7ca214c90f1f3e4481d67cdcda
nooose <hd15807@gmail.com>
2022-12-18 05:12:11 +0900

    #60 - 대댓글 비즈니스 로직 구현
    부모 자식 관계를 만들면서 entity <--> dto 간 변환 로직,
    댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법,
    댓글과 대댓글을 db 데이터로부터 변환할 때
    서로 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- src/main/java/com/project/board/dto/response/ArticleWithCommentsResponse.java
+++ src/main/java/com/project/board/dto/response/ArticleWithCommentsResponse.java
@@ -1,11 +1,12 @@
 package com.project.board.dto.response;
 
+import com.project.board.dto.ArticleCommentDto;
 import com.project.board.dto.ArticleWithCommentsDto;
 import com.project.board.dto.HashtagDto;
 
 import java.time.LocalDateTime;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public record ArticleWithCommentsResponse(
@@ -41,10 +42,29 @@ public record ArticleWithCommentsResponse(
                 dto.userAccountDto().email(),
                 nickname,
                 dto.userAccountDto().userId(),
-                dto.articleCommentDtos().stream()
-                        .map(ArticleCommentResponse::from)
-                        .collect(Collectors.toCollection(LinkedHashSet::new))
+                organizeChildComments(dto.articleCommentDtos())
         );
     }
 
+    private static Set<ArticleCommentResponse> organizeChildComments(Set<ArticleCommentDto> dtos) {
+        Map<Long, ArticleCommentResponse> map = dtos.stream()
+                .map(ArticleCommentResponse::from)
+                .collect(Collectors.toMap(ArticleCommentResponse::id, Function.identity()));
+
+        map.values().stream()
+                .filter(ArticleCommentResponse::hasParentComment)
+                .forEach(comment -> {
+                    ArticleCommentResponse parentComment = map.get(comment.parentCommentId());
+                    parentComment.childComments().add(comment);
+                });
+
+        return map.values().stream()
+                .filter(comment -> !comment.hasParentComment())
+                .collect(Collectors.toCollection(() ->
+                        new TreeSet<>(Comparator.comparing(ArticleCommentResponse::createdAt)
+                                .reversed()
+                                .thenComparing(ArticleCommentResponse::id)
+                        )
+                ));
+    }
 }
