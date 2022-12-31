e506c1275a378966b6f01d0bfa50e03afc9c2530
nooose <hd15807@gmail.com>
2022-12-31 21:04:12 +0900

    #69 - 인증 설정 업데이트: OAuth 인증 내용 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/test/java/com/project/board/config/TestSecurityConfig.java
+++ src/test/java/com/project/board/config/TestSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.project.board.config;
 
 import com.project.board.domain.UserAccount;
+import com.project.board.dto.UserAccountDto;
 import com.project.board.repository.UserAccountRepository;
+import com.project.board.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
@@ -13,17 +15,24 @@ import static org.mockito.BDDMockito.given;
 
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
-    @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetup() {
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private static UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
                 "nooseTest",
                 "pw",
                 "noose-test@email.com",
                 "noose-test",
                 "test memo"
-        )));
+        );
     }
 
 }
